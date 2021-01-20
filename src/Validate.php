5302361c9331a75d0d2b9f051cc5e1638dd0a9de
alexzy <1003057931@qq.com>
2021-01-20 17:28:29 +0800

    init

--- src/Validate.php
+++ src/Validate.php
@@ -4,6 +4,8 @@ namespace Alexzy\HyperfValidate;
 
 use Alexzy\HyperfValidate\Exception\ValidateException;
 use Closure;
+use Hyperf\HttpMessage\Upload\UploadedFile;
+use Hyperf\Utils\Str;
 
 class Validate
 {
@@ -135,6 +137,39 @@ class Validate
         'fileExt' => 'extensions to upload is not allowed',
         'fileMime' => 'mimetype to upload is not allowed',
         'not_rules' => 'not conform to the rules'
+    ];
+
+    /**
+     * @var array Filter_var 规则
+     */
+    protected $filter = [
+        'email' => FILTER_VALIDATE_EMAIL,
+        'ip' => [FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6],
+        'integer' => FILTER_VALIDATE_INT,
+        'url' => FILTER_VALIDATE_URL,
+        'macAddr' => FILTER_VALIDATE_MAC,
+        'float' => FILTER_VALIDATE_FLOAT,
+    ];
+
+    /**
+     * @var array 验证正则定义
+     */
+    protected $regex = [];
+
+    /**
+     * @var string[] 默认正则规则
+     */
+    protected $defaultRegex = [
+        'alpha' => '/^[A-Za-z]+$/',
+        'alphaNum' => '/^[A-Za-z0-9]+$/',
+        'alphaDash' => '/^[A-Za-z0-9\-\_]+$/',
+        'chs' => '/^[\x{4e00}-\x{9fa5}]+$/u',
+        'chsAlpha' => '/^[\x{4e00}-\x{9fa5}a-zA-Z]+$/u',
+        'chsAlphaNum' => '/^[\x{4e00}-\x{9fa5}a-zA-Z0-9]+$/u',
+        'chsDash' => '/^[\x{4e00}-\x{9fa5}a-zA-Z0-9\_\-]+$/u',
+        'mobile' => '/^1[3-9]\d{9}$/',
+        'idCard' => '/(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}$)/',
+        'zip' => '/\d{6}/',
     ];
 
     /**
@@ -448,6 +483,25 @@ class Validate
     }
 
     /**
+     * 判断是否为图片
+     * @param $image
+     * @return false|int|mixed
+     */
+    protected function getImageType($image)
+    {
+        if (function_exists('exif_imagetype')) {
+            return exif_imagetype($image);
+        }
+
+        try {
+            $info = getimagesize($image);
+            return $info ? $info[2] : false;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    /**
      * 错误信息数组处理
      * @param array $msg
      * @param $rule
@@ -501,5 +555,111 @@ class Validate
         }
 
         return $msg;
+    }
+
+    /**
+     * 验证字段值是否为有效格式
+     * @param $value
+     * @param string $rule
+     * @param array $data
+     * @return bool
+     */
+    public function is($value, string $rule, array $data = []): bool
+    {
+        switch (Str::camel($rule)) {
+            case 'require':
+                // 必须
+                $result = !empty($value) || '0' == $value;
+                break;
+            case 'accepted':
+                // 接受
+                $result = in_array($value, ['1', 'on', 'yes']);
+                break;
+            case 'date':
+                // 是否是一个有效日期
+                $result = false !== strtotime($value);
+                break;
+            case 'activeUrl':
+                // 是否为有效的网址
+                $result = checkdnsrr($value);
+                break;
+            case 'boolean':
+            case 'bool':
+                // 是否为布尔值
+                $result = in_array($value, [true, false, 0, 1, '0', '1'], true);
+                break;
+            case 'number':
+                $result = ctype_digit((string)$value);
+                break;
+            case 'alphaNum':
+                $result = ctype_alnum($value);
+                break;
+            case 'array':
+                // 是否为数组
+                $result = is_array($value);
+                break;
+            case 'file':
+                $result = $value instanceof UploadedFile;
+                break;
+            case 'image':
+                $result = $value instanceof UploadedFile && in_array($this->getImageType($value->getRealPath()), [1, 2, 3, 6]);
+                break;
+            default:
+                if (isset($this->type[$rule])) {
+                    // 注册的验证规则
+                    $result = call_user_func_array($this->type[$rule], [$value]);
+                } elseif (function_exists('ctype_' . $rule)) {
+                    // ctype_ 方法验证规则
+                    $ctypeFun = 'ctype_' . $rule;
+                    $result = $ctypeFun($value);
+                } elseif (isset($this->filter[$rule])) {
+                    // Filter_var验证规则
+                    $result = $this->filter($value, $this->filter[$rule]);
+                } else {
+                    $result = $this->regex($value, $rule);
+                }
+        }
+        return $result;
+    }
+
+    /**
+     * 使用filter_var方式验证
+     * @param $value
+     * @param $rule
+     * @return bool
+     */
+    public function filter($value, $rule): bool
+    {
+        if (is_string($rule) && strpos($rule, ',')) {
+            [$rule, $param] = explode(',', $rule);
+        } elseif (is_array($rule)) {
+            $param = $rule[1] ?? null;
+            $rule = $rule[0];
+        } else {
+            $param = null;
+        }
+        return false !== filter_var($value, is_int($rule) ? $rule : filter_id($rule), $param);
+    }
+
+    /**
+     * 使用正则验证数据
+     * @param $value
+     * @param $rule
+     * @return bool
+     */
+    public function regex($value, $rule): bool
+    {
+        if (isset($this->regex[$rule])) {
+            $rule = $this->regex[$rule];
+        } elseif (isset($this->defaultRegex[$rule])) {
+            $rule = $this->defaultRegex[$rule];
+        }
+
+        if (is_string($rule) && 0 !== strpos($rule, '/') && !preg_match('/\/[imsU]{0,4}$/', $rule)) {
+            // 不是正则表达式则两端补上/
+            $rule = '/^' . $rule . '$/';
+        }
+
+        return is_scalar($value) && 1 === preg_match($rule, (string)$value);
     }
 }
